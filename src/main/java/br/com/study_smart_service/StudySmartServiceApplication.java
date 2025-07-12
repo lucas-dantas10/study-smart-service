ca5c6ed5facb4e84d292156e6574ee5f4372ad37
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2025-07-12 11:47:26 -0300

    initial commit

---
+++ src/main/java/br/com/study_smart_service/StudySmartServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.study_smart_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySmartServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySmartServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/study_smart_service/StudySmartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.study_smart_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySmartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
