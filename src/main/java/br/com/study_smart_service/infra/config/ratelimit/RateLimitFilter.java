4f058516aaf3761909a44f5f905e764494683919
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2025-08-14 22:32:18 -0300

    feat: add rate limit config in application.yaml

--- src/main/java/br/com/study_smart_service/infra/config/ratelimit/RateLimitFilter.java
+++ src/main/java/br/com/study_smart_service/infra/config/ratelimit/RateLimitFilter.java
@@ -3,6 +3,8 @@ package br.com.study_smart_service.infra.config.ratelimit;
 import io.github.bucket4j.Bucket;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -11,7 +13,14 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
+@Slf4j
 public class RateLimitFilter implements Filter {
+
+    @Value("${rate-limit.minutes}")
+    private int minutes;
+
+    @Value("${rate-limit.tokens}")
+    private int tokens;
 
     private final Map<String, Bucket> buckets = new ConcurrentHashMap<>();
 
@@ -23,6 +32,7 @@ public class RateLimitFilter implements Filter {
         Bucket bucket = buckets.computeIfAbsent(ip, this::newBucket);
 
         if (!bucket.tryConsume(1)) {
+            log.error("Muitas solicitações: {}", bucket.getAvailableTokens());
             HttpServletResponse httpResponse = (HttpServletResponse) response;
             httpResponse.setStatus(429);
             httpResponse.getWriter().write("Muitas solicitações");
@@ -35,7 +45,7 @@ public class RateLimitFilter implements Filter {
 
     private Bucket newBucket(String key) {
         return Bucket.builder()
-                .addLimit(limit -> limit.capacity(10).refillGreedy(10, Duration.ofMinutes(1)))
+                .addLimit(limit -> limit.capacity(tokens).refillGreedy(tokens, Duration.ofMinutes(minutes)))
                 .build();
     }
 }
