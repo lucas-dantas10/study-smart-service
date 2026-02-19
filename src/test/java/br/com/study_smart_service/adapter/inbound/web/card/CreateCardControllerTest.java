2d95bc30c43a22ca77495b91531dfee93dc0619e
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2026-02-18 23:21:23 -0300

    chore: add CI

--- src/test/java/br/com/study_smart_service/adapter/inbound/web/card/CreateCardControllerTest.java
+++ src/test/java/br/com/study_smart_service/adapter/inbound/web/card/CreateCardControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -24,6 +25,7 @@ import static org.mockito.ArgumentMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@ActiveProfiles("test")
 @WebMvcTest(CreateCardController.class)
 @AutoConfigureMockMvc(addFilters = false)
 class CreateCardControllerTest {
