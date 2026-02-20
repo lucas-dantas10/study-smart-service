896436fc46c00c92dfa36b93a45516bd679b4251
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2026-02-20 15:37:31 -0300

    chore: adjust changes in checkstyle

--- src/main/java/br/com/study_smart_service/adapter/outbound/card/repository/JpaCardRepository.java
+++ src/main/java/br/com/study_smart_service/adapter/outbound/card/repository/JpaCardRepository.java
@@ -18,28 +18,28 @@ public interface JpaCardRepository extends JpaRepository<CardEntity, UUID> {
 
     @Query("""
         SELECT c
-             FROM CardEntity c
-         JOIN c.deck d
-         LEFT JOIN ReviewEntity r ON r.card = c AND r.user.id = :userId
-         WHERE d.id = :deckId AND (
-                 r.id IS NULL
-                 OR
-                 (
-                   r.createdAt = (
-                       SELECT MAX(r2.createdAt)
-                       FROM ReviewEntity r2
-                       WHERE r2.card = c AND r2.user.id = :userId
-                   )
-                   AND r.nextReviewAt <= CURRENT_DATE
-                 )
-               )
-         ORDER BY
-             CASE
-                 WHEN r.nextReviewAt IS NULL THEN 0
-                 ELSE 1
-             END,
-             r.nextReviewAt ASC
-    """)
+        FROM CardEntity c
+        JOIN c.deck d
+        LEFT JOIN ReviewEntity r ON r.card = c AND r.user.id = :userId
+        WHERE d.id = :deckId AND (
+                r.id IS NULL
+                OR
+                (
+                    r.createdAt = (
+                        SELECT MAX(r2.createdAt)
+                        FROM ReviewEntity r2
+                        WHERE r2.card = c AND r2.user.id = :userId
+                    )
+                    AND r.nextReviewAt <= CURRENT_DATE
+                )
+        )
+        ORDER BY
+            CASE
+                WHEN r.nextReviewAt IS NULL THEN 0
+                ELSE 1
+            END,
+            r.nextReviewAt ASC
+        """)
     List<CardEntity> findAllStudyCardsByDeckIdAndUserId(UUID deckId, UUID userId);
 
     @Query("SELECT card FROM CardEntity card " +
